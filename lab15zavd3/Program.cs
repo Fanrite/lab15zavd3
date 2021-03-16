1d3d88fb3d7d65219f235b177c20f45709074a5f
Fanrite <76695059+Fanrite@users.noreply.github.com>
2021-03-16 21:54:39 +0200

    Добавьте файлы проекта.

---
+++ lab15zavd3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.Globalization;
+
+namespace lab15zavd3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string output = "";
+            string txt;
+
+            txt = Interaction.InputBox("...", "Введіть текст: ", ".капец.");
+            TextInfo ti = CultureInfo.CurrentCulture.TextInfo;
+            char[] arr = txt.ToCharArray();
+            
+            arr[0] = ti.ToUpper(arr[0]);
+
+            for (int i = 0; i < arr.Length - 1; i++)
+                if (arr[i] == ' ')
+                    arr[i+1] = ti.ToUpper(arr[i+1]);
+
+            foreach (char j in arr)
+                output += j;
+
+            MessageBox.Show($"Вхідний рядок: {txt}\nОброблений рядок: {output}", "Результат", MessageBoxButtons.OK);
+
+        }
+    }
+}
